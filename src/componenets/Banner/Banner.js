42551736efc15ef753c1be28ce5854e997112863
bhasi <anilbhasib@gmail.com>
2022-01-07 09:23:36 +0530

    trending movies added

--- src/componenets/Banner/Banner.js
+++ src/componenets/Banner/Banner.js
@@ -1,25 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
 import axios from '../../apis/axios'
-import {apiKey} from '../../constants/constants'
+import {apiKey,imageUrl} from '../../constants/constants'
 
 function Banner() {
+    const [movie, setMovie] = useState([])
     useEffect(() => {
         
      axios.get(`trending/all/week?api_key=${apiKey}&language=en-US`)
      .then((res)=>{
-         console.log(res.data.results[0]);
+         console.log(res.data.results[Math.floor(Math.random() * 20)]);
+         setMovie(res.data.results[Math.floor(Math.random() * 20)])
      })   
     }, [])
     return (
-        <div className='banner'>
+        <div 
+        style={{backgroundImage:`url(${imageUrl+movie.backdrop_path})`}}
+        className='banner'>
             <div className="content">
-                <h1 className="title">Movie Name</h1>
+                <h1 className="title">{movie.title}</h1>
                 <div className="banner_buttons">
                     <div className="button">Play</div>
                     <div className="button">My List</div>
                 </div>
-                <h1 className="description">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</h1>
+                <h1 className="description">{movie.overview}</h1>
             </div>
         <div className="fade_bottom"></div>
         </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3'
 export const apiKey = 'e8c60d9f335bd00b387116f68c295274'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
