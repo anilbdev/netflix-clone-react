caa16185c7bc40d8d2c2353918c6cae2f541de30
bhasi <anilbhasib@gmail.com>
2022-01-07 11:37:11 +0530

    youtube react added

--- src/componenets/RowPost/RowPost.js
+++ src/componenets/RowPost/RowPost.js
@@ -1,10 +1,12 @@
 import React,{useEffect,useState} from 'react'
+import YouTube from 'react-youtube'
 import './RowPost.css'
 import axios  from '../../apis/axios'
-import{ imageUrl} from '../../constants/constants'
+import{ imageUrl,apiKey} from '../../constants/constants'
 
 function RowPost({title,url}) {
     const [movies, setMovies] = useState([])
+    const [urlId,setUrlId] = useState('')
     useEffect(() => {
       axios.get(url).then((res)=>{
           console.log(res.data.results);
@@ -13,16 +15,41 @@ function RowPost({title,url}) {
           alert('Network Error')
       })
     }, [])
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        }}
+
+    const handleMovieTrailer = (id)=>{
+        console.log('clicked id >-->',id);
+        axios.get(`movie/${id}/videos?api_key=${apiKey}&language=en-US`).then((res)=>{
+            console.log(res.data);
+            if(res.data.results.length !== 0){
+                setUrlId(res.data.results[0])
+            }else{
+                alert('Trailer not available')
+            }
+        }).catch((err)=>{
+            alert('Trailer not available')
+        })
+    }
     return (
         <div className="row">
             <h2>{title}</h2>
             <div className="posters">
                 {
-                    movies.map((movie)=><img className='poster' src={`${imageUrl+movie.poster_path}`} alt="movie-image" />)
+                    movies.map((movie)=><img onClick={()=>handleMovieTrailer(movie.id)} className='poster' src={`${imageUrl+movie.poster_path}`} alt="movie-image" />)
                 }
             
             
             </div>
+            {
+                urlId && <YouTube videoId={urlId.key} opts={opts}  />
+            }
+            
         </div>
     )
 }
